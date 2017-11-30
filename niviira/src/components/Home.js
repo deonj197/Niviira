eb79ac82b35c53cf11ebfe7103f675b657abe750
deonj197 <deonj197@gmail.com>
2017-11-30 02:35:17 -0500

    Setting up homepage and other boilerplate

---
+++ niviira/src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import '../styles/Home.css'
+
+class Home extends Component {
+  render() {
+    
+  }
+}
---
+++ niviira/src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../../styles/Header.css'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <header>
+          <h1>Test</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export { Header };
